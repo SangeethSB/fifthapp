290019b00e998f97f7bb2250d460b6b344f8d69c
SangeethSB <ssb.sangeeth@gmail.com>
2018-10-04 14:20:04 +0530

    two components html form and html table added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HtmlTableComponent } from './components/html-table/html-table.component';
+import { HtmlFormComponent } from './components/html-form/html-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HtmlTableComponent,
+    HtmlFormComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/html-form/html-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-html-form',
+  templateUrl: './html-form.component.html',
+  styleUrls: ['./html-form.component.css']
+})
+export class HtmlFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
